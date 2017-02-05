b4e36932812ad616abbf1e4f39bd4b9978981b88
Benjamin Allen <benjaminallen1985@yahoo.com>
2017-02-04 20:19:22 -0600

    First Commit

---
+++ ContactProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactProject.Startup))]
+namespace ContactProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
