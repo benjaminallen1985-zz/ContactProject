11c462d940c96c9bcc046c871a1d13c6c70a7fb2
Benjamin Allen <benjaminallen1985@yahoo.com>
2017-02-05 02:10:38 -0600

    Added Scaffolding for Controllers

---
+++ ContactProject/Models/ContactProjectContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactProject.Models
+{
+    public class ContactProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ContactProjectContext() : base("name=ContactProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ContactProject.Models.Contact> Contacts { get; set; }
+    }
+}
