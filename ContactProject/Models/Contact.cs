0be9129221e3d175abe9df6fe015092ad491896d
Benjamin Allen <benjaminallen1985@yahoo.com>
2017-02-07 15:31:25 -0600

    First fix attempt for out of range issue with datetime inputs

--- ContactProject/Models/Contact.cs
+++ ContactProject/Models/Contact.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 
@@ -14,11 +16,20 @@ namespace ContactProject.Models
         public string Email { get; set; }
         public string PhonePrimary { get; set; }
         public string PhoneSecondary { get; set; }
-        public DateTime Birthday { get; set; }
+        [DbDateRange]
+        public DateTime? Birthday { get; set; }
         public string StreetAddress1 { get; set; }
         public string StreetAddress2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
+    }
+
+    public class DbDateRangeAttribute
+    : RangeAttribute
+    {
+        public DbDateRangeAttribute()
+            : base(typeof(DateTime), SqlDateTime.MinValue.Value.ToShortDateString(), SqlDateTime.MaxValue.Value.ToShortDateString())
+        { }
     }
 }
